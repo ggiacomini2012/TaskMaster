1c0a628885280a5a223522c7f55f655aa81e022c
Guilherme Teixeira <ggiacomini2012@gmail.com>
2024-09-22 15:51:04 -0300

    fix: corrige configuração do ESLint no backend e atualiza dependências

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,19 +1,27 @@
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import babelEslintParser from "@babel/eslint-parser";
+
 export default [
   {
-    ignores: ["node_modules/**"], // Ignora a pasta node_modules
+    ignores: ["node_modules/**"],
   },
   {
-    files: ["**/*.js"], // Aplica as configurações aos arquivos JavaScript
+    files: ["src/**/*.js"],
     languageOptions: {
-      ecmaVersion: "latest", // Define a versão mais recente do ECMAScript
-      sourceType: "module", // Define o tipo de módulo como "module"
+      ecmaVersion: 2021,
+      sourceType: "module",
+      parser: babelEslintParser,
+      parserOptions: {
+        requireConfigFile: false,
+      },
     },
     plugins: {
-      prettier: require("eslint-plugin-prettier"), // Adiciona o plugin do Prettier ao ESLint
+      prettier: eslintPluginPrettier,
     },
-    extends: ["eslint:recommended", "plugin:prettier/recommended"], // Extende as configurações recomendadas do ESLint e do Prettier
     rules: {
-      "prettier/prettier": "error", // Define que erros do Prettier devem ser tratados como erros do ESLint
+      ...eslintConfigPrettier.rules, // Inclui as regras do Prettier
+      "prettier/prettier": "error",
     },
   },
 ];
