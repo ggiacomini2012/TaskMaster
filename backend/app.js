c6daa7a887e9fed9c3822d73529958ec625aedcd
Guilherme Teixeira <ggiacomini2012@gmail.com>
2024-09-27 18:53:33 -0300

    refactor(api): atualizar lógica de servidor em app.js e index.js, e ajustes no package.json

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
+// const authRoutes = require('./routes/authRoutes');
+// app.use('/api/auth', authRoutes);
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 // Habilitar CORS para permitir requisições de outros domínios
 app.use(cors());
@@ -12,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 // Rotas de autenticação
-app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).send({ message: 'API rodando...' });
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,42 @@
-const app = require('./app');
 const sequelize = require('./config/db');
 const User = require('./models/userModel');
 const Task = require('./models/taskModel');
-require('dotenv').config({ path: './.env' });
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+require('dotenv').config();
 
+const app = express();
 const port = process.env.PORT || 5000;
+
+// Habilitar CORS para permitir requisições de outros domínios
+app.use(cors());
+
+// Middleware para processar JSON no corpo das requisições
+app.use(express.json());
+
+// Rotas de autenticação
+app.use('/api/auth', authRoutes);
+
+// Rota básica
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'API rodando...' });
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error);
+    res.status(500).send({ message: 'Erro ao buscar usuários' });
+  }
+});
+
+// Iniciar o servidor
+// app.listen(port, () => {
+//   console.log(`Servidor rodando na porta ${port}`);
+// });
 
 sequelize.sync({ alter: true })
   .then(() => {
@@ -16,3 +48,5 @@ sequelize.sync({ alter: true })
   .catch(err => {
     console.error('Erro ao sincronizar as tabelas:', err);
   });
+
+module.exports = app;
