eac61b980c09530a6dfc5d463f07d073e48e74a0
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2024-10-19 13:47:15 -0300

    test(frontend/api): test communication between frontend and API with database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import UsersList from './components/UsersList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá Bob aqui!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bem-vindo ao TaskMaster</h1>
+      <UsersList />  {/* Usando o componente aqui */}
     </div>
   );
 }
---
+++ frontend/src/components/UsersList.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://task-master-delta-eosin.vercel.app/users');
+        const data = await response.json();
+        console.log(data)
+        setUsers(data);
+      } catch (error) {
+        console.error('Erro ao buscar usuários:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Usuários</h1>
+      {loading ? (
+        <p>Carregando usuários...</p>
+      ) : users.length > 0 ? (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+            <p><strong>Username:</strong> {user.username}</p>
+            <p><strong>Password:</strong> {user.password}</p> {/* Remova essa linha se não quiser mostrar a senha */}
+            <p><strong>Criado em:</strong> {new Date(user.createdAt).toLocaleString()}</p>
+            <p><strong>Atualizado em:</strong> {new Date(user.updatedAt).toLocaleString()}</p>
+            <hr />
+          </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Nenhum usuário encontrado.</p>
+      )}
+    </div>
+  );
+};
+
+export default UsersList;
